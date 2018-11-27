fd687e3587db75cf21d8abc934c273cd89547d78
unknown <118224223@umail.ucc.ie>
2018-11-27 19:30:04 +0000

    sixcommand

--- index.php
+++ index.php
@@ -10,6 +10,69 @@
 	</head>
 
 	<body>
+	<!--Navbar-->
+<span
+><nav class="navbar navbar-expand-lg navbar-dark bg-blue">
+<a class="navbar-brand" href="index.html"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<a class="navbar-image" href="index.html">
+			<img src="./Images/logo.png" width= "300" height = "90" alt="logo" style = opacity="1">
+		</a>
+</span>
+		
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html" style = "background-color: yellow; color:blue";>Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="./FAQ/FAQ.html">FAQ</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="./Knowledgebase/KnowledgeHome.html">KnowledgeBase </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="./ticketingsystem/TicketHome.html">Ticketing System</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav> 		 
+		 <h1>Customer Support Portal</h1>
+
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="nav navbar-nav mr-auto">
+	  <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+	  
+		
+		<li class="nav-item">
+			<a class="nav-link" href="./FAQ/FAQ.html">FAQ</a>
+			</li>
+
+		
+			<li class="nav-item">
+			<a class="nav-link" href="./KnowledgeBase/KnowledgeHome.html">Knowledge Base</a>
+			</li>
+					<li class="nav-item" ">
+			<a class=" nav-link" href="./TicketingSystem/TicketHome.html">Create Ticket</a>
+			</li>
+		</ul>
+	</div>
+
+</nav>
+
+
+
+<br>
 
 	<h1> Killian O'Keeffe</h1>
 		<p> 50 is the king </p>
